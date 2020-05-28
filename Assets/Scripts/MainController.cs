a16e72023aa39c822e954139a8e90e59efe7de6b
SybelBlue <lbc.dna@gmail.com>
2020-05-28 09:26:16 -0700

    docs on MainController

--- Assets/Scripts/MainController.cs
+++ Assets/Scripts/MainController.cs
@@ -1,12 +1,31 @@
 ﻿using UnityEngine;
-using CustomInput;
 
 public class MainController : MonoBehaviour
 {
+    /// <summary>
+    /// The DisplayController that is in charge of loading the layout
+    /// </summary>
     public DisplayController displayController;
-    public InputFieldController inputPanel;
-    public RectTransform displayRect, indicatorRect;
 
+    /// <summary>
+    /// The main input source
+    /// </summary>
+    public CustomInput.InputFieldController inputPanel;
+
+    /// <summary>
+    /// The transform of the layout display
+    /// </summary>
+    public RectTransform displayRect;
+
+    /// <summary>
+    /// The transform of the indicator
+    /// </summary>
+    public RectTransform indicatorRect;
+
+    /// <summary>
+    /// True if no touch input is provided
+    /// </summary>
+    /// <returns>no touch input</returns>
     public static bool NoTouches()
     {
         return Input.touchCount == 0;
@@ -47,7 +66,12 @@ public class MainController : MonoBehaviour
             Debug.Log(groupText + exactItem.data);
         }
     }
-    public void OnSliderValueChange(int value)
+
+    /// <summary>
+    /// Callback for when the InputFieldController value changes due to user input
+    /// </summary>
+    /// <param name="value">the new value</param>
+    public void OnInputValueChange(int value)
     {
         float width = displayRect.rect.width;
         var pos = indicatorRect.position;
