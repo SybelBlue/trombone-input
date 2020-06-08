422ded4a0605ba3c8d673b51ed5d6d200f21861c
Zahara Spilka <zspilka@macalester.edu>
2020-06-08 11:00:54 -0500

    Changes just from running the code

--- Assets/Scripts/Layouts/CircularLayout.cs
+++ Assets/Scripts/Layouts/CircularLayout.cs
@@ -85,6 +85,10 @@ public class CircularLayout : LayoutGroup {
                 DrivenTransformProperties.Pivot);
                 Vector3 vPos = new Vector3(Mathf.Cos(fAngle * Mathf.Deg2Rad), Mathf.Sin(fAngle * Mathf.Deg2Rad), 0);
                 child.localPosition = vPos * fDistance;
+                // Vector3 vScal = new Vector3(0.99f, 1.0f, 0);
+                // child.localScale = vScal;
+
+
 
                 //Force objects to be center aligned, this can be changed however I'd suggest you keep all of the objects with the same anchor points.
                 child.anchorMin = child.anchorMax = child.pivot = new Vector2(0.5f, 0.5f);
