904a0969f409e8116b73a2c66394f6cb34116c94
SybelBlue <lbc.dna@gmail.com>
2020-06-18 16:06:09 -0500

    added third rotation indicator

--- Assets/Scripts/Controllers/StylusIndicator.cs
+++ Assets/Scripts/Controllers/StylusIndicator.cs
@@ -1,12 +1,13 @@
 ﻿public class StylusIndicator : UnityEngine.MonoBehaviour
 {
-    public UnityEngine.UI.Slider xSlider, zSlider;
+    public UnityEngine.UI.Slider xSlider, ySlider, zSlider;
 
     public StylusModelController modelController;
 
     void Update()
     {
         xSlider.value = modelController.normalizedAngles.x;
+        ySlider.value = modelController.normalizedAngles.y;
         zSlider.value = modelController.normalizedAngles.z;
     }
 }
