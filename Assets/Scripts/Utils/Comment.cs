f3b43d5b88f268b9f68b5122540fcf9f5e2d5dac
SybelBlue <lbc.dna@gmail.com>
2020-06-30 11:53:16 -0500

    got Comment.cs to cooperate

--- Assets/Scripts/Utils/Comment.cs
+++ Assets/Scripts/Utils/Comment.cs
@@ -1,22 +1,10 @@
 ﻿using UnityEngine;
-using UnityEditor;
 
 namespace Utils
 {
     public class Comment : MonoBehaviour
-    { }
-}
-
-#if (UNITY_EDITOR)
-namespace CustomEditor
-{
-    [UnityEditor.CustomEditor(typeof(Utils.Comment))]
-    public class CommentEditor : Editor
     {
-        public override void OnInspectorGUI()
-        {
-            EditorGUILayout.TextArea("");
-        }
+        [TextArea]
+        public string notes;
     }
 }
-#endif
