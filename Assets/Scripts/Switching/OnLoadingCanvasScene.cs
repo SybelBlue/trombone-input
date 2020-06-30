2a082d746ee3ce5fd7f67dc16dd307d963fd15b1
Zahara Spilka <zspilka@macalester.edu>
2020-06-30 09:12:21 -0500

    Update OnLoadingCanvasScene.cs

--- Assets/Scripts/Switching/OnLoadingCanvasScene.cs
+++ Assets/Scripts/Switching/OnLoadingCanvasScene.cs
@@ -3,43 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class OnLoadingCanvasScene : MonoBehaviour
+public class OnLoadingCanvasScene : UnityEngine.MonoBehaviour
 {
-    // // Start is called before the first frame update
-    // void Start()
-    // {
-    //
-    // }
-    //
-    // // Update is called once per frame
-    // void Update()
-    // {
-    //
-    // }
-    // public void LeaveTrialScene()
-    // {
-    //   if(!SceneManager.GetActiveScene().name.Equals("Stylus Trial"))
-    //   {
-    //     SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(0)); //set's the active scene to the basic scene since the testObj scene is going to be unloaded.
-    //
-    //   }
-    //   SceneManager.sceneLoaded -= OnSceneLoaded;
-    //   //SceneManager.LoadScene(0); //Transition Scene
-    //   SceneManager.UnloadSceneAsync(sceneIndex);
-    //   //SceneManager.LoadScene(0, LoadSceneMode.Single);
-    //
-    // }
-
     public void startTrialScene()
     {
-      // if (!SceneManager.GetActiveScene().name.Equals("Stylus Trial"))
-      // {
         UnityEngine.SceneManagement.SceneManager.LoadScene("_STRIALS", LoadSceneMode.Additive);
-      // }
 
     }
 
-    // SceneManager.LoadScene(sceneIndex, LoadSceneMode.Additive);
-    // SceneManager.sceneLoaded += OnSceneLoaded;
 
 }
+
+// // Start is called before the first frame update
+// void Start()
+// {
+//
+// }
+//
+// // Update is called once per frame
+// void Update()
+// {
+//
+// }
+// public void LeaveTrialScene()
+// {
+//   if(!SceneManager.GetActiveScene().name.Equals("Stylus Trial"))
+//   {
+//     SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(0)); //set's the active scene to the basic scene since the testObj scene is going to be unloaded.
+//
+//   }
+//   SceneManager.sceneLoaded -= OnSceneLoaded;
+//   //SceneManager.LoadScene(0); //Transition Scene
+//   SceneManager.UnloadSceneAsync(sceneIndex);
+//   //SceneManager.LoadScene(0, LoadSceneMode.Single);
+//
+// }
+
+// // if (!SceneManager.GetActiveScene().name.Equals("Stylus Trial"))
+// // {
+//   UnityEngine.SceneManagement.SceneManager.LoadScene("_STRIALS", LoadSceneMode.Additive);
+// // }
+    // SceneManager.LoadScene(sceneIndex, LoadSceneMode.Additive);
+    // SceneManager.sceneLoaded += OnSceneLoaded;
