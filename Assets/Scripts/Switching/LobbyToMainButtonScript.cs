4e28ebd5d8d4aa83ae8fb87586eec1f45fe7b426
Zahara Spilka <zspilka@macalester.edu>
2020-06-30 08:32:40 -0500

    Update LobbyToMainButtonScript.cs

--- Assets/Scripts/Switching/LobbyToMainButtonScript.cs
+++ Assets/Scripts/Switching/LobbyToMainButtonScript.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LobbyToMainButtonScript : MonoBehaviour
 {
     public void changeToMain()
     {
+        // UnityEngine.SceneManagement.SceneManager.LoadScene("_STRIALS", LoadSceneMode.Additive);
         UnityEngine.SceneManagement.SceneManager.LoadScene("_STRIALS", LoadSceneMode.Additive);
     }
     // // Start is called before the first frame update
