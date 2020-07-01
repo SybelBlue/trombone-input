b701363f983ef11843cde7ab4d1cc2ff79f79b69
Zahara Spilka <zspilka@macalester.edu>
2020-07-01 09:51:00 -0500

    Got the button to disappear

---
+++ Assets/Scripts/Switching/ButtonDisappear.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonDisappear : MonoBehaviour
+{
+  GameObject button;
+  // public Button StartPracticeButton;
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+    //   button = GameObject.FindWithTag("StartButtonTag");
+    //   // ButtonClick();
+    //
+    //   // button.onClick.AddListener(Butt)
+    //   // Button sButton = StartPracticeButton.GetComponent<Button>();
+    //   // sButton.onClick.AddListener(TaskOnClick);
+    // }
+
+    public void ButtonClick()
+    {
+      button = GameObject.FindWithTag("StartButtonTag");
+      button.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
