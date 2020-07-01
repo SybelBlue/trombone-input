99865ab81e8dbf0f4ad55401d2d426f2f097e5c2
Zahara Spilka <zspilka@macalester.edu>
2020-07-01 13:05:46 -0500

    Update ButtonReappear.cs

--- Assets/Scripts/Switching/ButtonReappear.cs
+++ Assets/Scripts/Switching/ButtonReappear.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class ButtonReappear : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  public void ButtonReClick()
+  {
+    var newColor = new Color (235,255,255,1.0f);
+    button = GameObject.FindWithTag("StartButtonTag");
+    button.SetActive(false);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // skybox = GameObject.FindWithTag("MainCamera");
+    skybox = GetComponent<Camera>();
+    skybox = Camera.main;
+    skybox.clearFlags = CameraClearFlags.Skybox;
+    skybox.backgroundColor = newColor;
+
+    //TODO: Set skybox to EBFFFF
+    // skybox = GameObject.FindWithTag("MainCamera");
+    // skybox.SetBackgroundColor("EBFFFF");
+  }
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+    //
+    // }
+    //
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //
+    // }
 }
