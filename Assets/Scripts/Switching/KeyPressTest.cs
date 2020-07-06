2e51e5fe0ee96f5e2a8babb720020d25a80d0e70
Zahara Spilka <zspilka@macalester.edu>
2020-07-06 18:44:25 -0500

    Now when we press return on the keyboard we can switch between trials and lobby

---
+++ Assets/Scripts/Switching/KeyPressTest.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class KeyPressTest : MonoBehaviour
+{
+  public Button backToLobby;
+  public Button toTrials;
+  public Main other;
+  // var return : Keyeve
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      backToLobby = GameObject.FindGameObjectWithTag("JumbBackToLobbyTag").GetComponent<Button>();
+      toTrials = GameObject.FindGameObjectWithTag("StartButtonTag").GetComponent<Button>();
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      Scene scene = SceneManager.GetActiveScene();
+      Scene sceeneTwo = SceneManager.GetSceneByName("_STRIALS");
+      Debug.Log($"Active Scene: \"{scene.name}\".");
+
+      if(Input.GetKeyDown(KeyCode.Return))
+      {
+        // if(scene.name.Equals("_STRIALS").isLoaded())
+        if(sceeneTwo.isLoaded)
+        {
+          other.OnSceneAdvance();
+          Debug.LogWarning("OnSceneAdvance WORKED");
+          backToLobby.onClick.Invoke();
+          Debug.LogWarning("backtolobby");
+
+
+        }
+        else
+        {
+          // Main.OnSceneAdvance();
+          toTrials.onClick.Invoke();
+          Debug.LogWarning("toTrials WORKED");
+
+
+        }
+      }
+      // scene = SceneManager.GetActiveScene();
+      // Debug.Log($"Active Scene: \"{scene.name}\".");
+
+    }
+}
