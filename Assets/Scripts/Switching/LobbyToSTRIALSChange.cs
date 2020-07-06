deb4373dc25b63380da29c8fe965831893f07ece
Zahara Spilka <zspilka@macalester.edu>
2020-07-06 11:56:17 -0500

    Working on scene transitions with return

--- Assets/Scripts/Switching/LobbyToSTRIALSChange.cs
+++ Assets/Scripts/Switching/LobbyToSTRIALSChange.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+// using is = ButtonDisappear;
 
 namespace SceneSwitching
 {
     public class LobbyToSTRIALSChange : MonoBehaviour
     {
+      // public ButtonDisappear bd;
+      GameObject button;
+      public Camera skybox;
         // Start is called before the first frame update
         void Start()
         {
@@ -17,7 +22,10 @@ namespace SceneSwitching
         {
             if (CustomInput.Bindings.advanceToMain)//TODO: EXPLORE THIS
             {
-                ButtonDisappear.ButtonClick();
+              ButtonRETURN();
+              // bd.ButtonClick();
+                // GetComponent<StartPracticeButton>().onClick.Invoke();
+                // sn = GetComponent<StartPracticeButton>().onClick.Invoke();
                 UnityEngine.SceneManagement.SceneManager.LoadScene(Utils._STRIALS_name, LoadSceneMode.Additive);
 
 
@@ -26,5 +34,23 @@ namespace SceneSwitching
             // SceneManager.LoadScene("_MAIN");
 
         }
+
+        public void ButtonRETURN()
+        {
+            var newColor = new Color(235, 255, 255, 1.0f);
+            button = GameObject.FindWithTag("StartButtonTag");
+            button.SetActive(false);
+
+            // skybox = GameObject.FindWithTag("MainCamera");
+            skybox = GetComponent<Camera>();
+            skybox = Camera.main;
+            skybox.clearFlags = CameraClearFlags.Skybox;
+            skybox.backgroundColor = newColor;
+
+            //TODO: Set skybox to EBFFFF
+            // skybox = GameObject.FindWithTag("MainCamera");
+            // skybox.SetBackgroundColor("EBFFFF");
+        }
+
     }
 }
--- Assets/Scripts/Switching/STRIALSToLobbyChange.cs
+++ Assets/Scripts/Switching/STRIALSToLobbyChange.cs
@@ -5,6 +5,8 @@ namespace SceneSwitching
 {
     public class STRIALSToLobbyChange : MonoBehaviour
     {
+      GameObject buttontwo;
+      public Camera skybox;
         // Start is called before the first frame update
         void Start()
         {
@@ -16,12 +18,31 @@ namespace SceneSwitching
         {
             if (CustomInput.Bindings.returnToLobby)
             {
+              ButtonReRETURN();
                 UnityEngine.SceneManagement.SceneManager.UnloadSceneAsync(Utils._STRIALS_name);
                 // UnityEngine.SceneManagement.SceneManager.UnloadSceneAsync("Trials");
 
                 // SceneManager.LoadScene("Lobby");
             }
 
+        }
+
+
+        public void ButtonReRETURN()
+        {
+            var newColor = new Color(184, 158, 195, 0.0f);
+            buttontwo = GameObject.FindWithTag("StartButtonTag");
+            buttontwo.SetActive(true);
+
+            // skybox = GameObject.FindWithTag("MainCamera");
+            skybox = GetComponent<Camera>();
+            skybox = Camera.main;
+            skybox.clearFlags = CameraClearFlags.Skybox;
+            skybox.backgroundColor = newColor;
+
+            //TODO: Set skybox to EBFFFF
+            // skybox = GameObject.FindWithTag("MainCamera");
+            // skybox.SetBackgroundColor("EBFFFF");
         }
     }
 }
