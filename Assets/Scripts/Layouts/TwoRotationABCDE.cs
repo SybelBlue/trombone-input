151b4162c3bba4f4c959ecdbb6d10d988e539dba
Bret Jackson <bjackson@macalester.edu>
2020-07-06 15:57:52 +0100

    Fixed angle override for tworotation layout

--- Assets/Scripts/Layouts/TwoRotationABCDE.cs
+++ Assets/Scripts/Layouts/TwoRotationABCDE.cs
@@ -8,12 +8,6 @@ namespace CustomInput
         {
             public override bool usesSlider => false;
 
-            [SerializeField]
-            protected Vector3 minAngle, maxAngle;
-
-            public override (Vector3 minima, Vector3 maxima)? StylusRotationBounds()
-                 => (minAngle, maxAngle);
-
             protected override int? InnerIndex(InputData data, int parentSize)
                 => Utils.Static.NormalizedIntoIndex(1 - data.normalizedAngles.x, parentSize);
         }
