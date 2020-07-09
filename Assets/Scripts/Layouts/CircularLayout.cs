0efe0197ab261b955edb6169a9b71e7d4da4d754
Zahara Spilka <zspilka@macalester.edu>
2020-07-09 16:03:45 -0500

    Added Copyright/citation for circular layout UI

--- Assets/Scripts/Layouts/CircularLayout.cs
+++ Assets/Scripts/Layouts/CircularLayout.cs
@@ -1,3 +1,29 @@
+/*
+Radial Layout Group by Just a Pixel (Danny Goodayle) - http://www.justapixel.co.uk
+Copyright (c) 2015
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+TODO: How to properly cite in code that we used code found on the web as our
+      starting point for this layout
+      actual website the code is found is http://www.justapixel.co.uk/2015/09/14/radial-layouts-nice-and-simple-in-unity3ds-ui-system/
+      GitHub Repo: https://gist.github.com/DGoodayle/aa62a344aa83e5342175
+*/
+
 namespace UnityEngine.UI{
   [AddComponentMenu("Layout/Circular Layout Group", 155)]
 
